be21d20d6119e4bbe7e3a8a2210da649ac188cd3
Suizzle <andygolay@gmail.com>
2023-12-26 18:04:07 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ js/constants.js
@@ -0,0 +1,23 @@
+module.exports = {
+  testnetContractAddress: "0xed9ebe0bba5ac978eeb6808c2d71a789dd97a760e43a33402761c27bd69a9284",
+  testnetLeaderboardAddress: "0x4ff7969fd02ac43d414a794476aa7c6701952a1b6b0ff8b8a17451e10af81f2d",
+  testnetMaintainerAddress: "0x1dff9c3642858921118c4f2d0581f17a8440b199aab7bf683e2556254151fc61",
+  mainnetContractAddress: "0x72f9c76421170b5a797432ba9e1b3b2e2b7cf6faa26eb955396c773af2479e1e",
+  mainnetLeaderboardAddress: "0xa834ebce466a79a3e2136c05fadce0322318051e0609f208a5d42cc04e0a67a3",
+  mainnetMaintainerAddress: "0x1d6d6770b9929e9d8233b31348f035a2a552d8427ae07d6413d1f88939f3807f",
+  tileNames: {
+    1: "Air",
+    2: "Mist",
+    3: "Fog",
+    4: "Dew",
+    5: "Droplet",
+    6: "Brook",
+    7: "Stream",
+    8: "Pond",
+    9: "River",
+    10: "Lake",
+    11: "Sea",
+    12: "Ocean",
+    13: "Sui"
+  }
+}
